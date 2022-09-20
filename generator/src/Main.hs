30a067df7bf4c56fb4a3e1573911234efcc07fde
Matthew Healy <matthew@liamhealy.xyz>
2022-09-20 23:30:17 +0200

    Reformat to 2 space indent

--- generator/src/Main.hs
+++ generator/src/Main.hs
@@ -20,80 +20,76 @@ config =
 
 main :: IO ()
 main = hakyllWith config $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/*" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile tailwindCssCompiler
+  match "css/*" $ do
+    route   idRoute
+    compile tailwindCssCompiler
 
-    match (fromList ["about.md", "contact.md"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match (fromList ["about.md", "contact.md"]) $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html"    postCtx
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx = listField "posts" postCtx (return posts)
+        <> constField "title" "Archives"
+        <> defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
 
 
-    match "index.md" $ do
-        route $ setExtension "html"
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
+  match "index.md" $ do
+    route $ setExtension "html"
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx = listField "posts" postCtx (return posts) <> defaultContext
 
-            pandocCompiler
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+      pandocCompiler
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
 
-    match "templates/*" $ compile templateBodyCompiler
+  match "templates/*" $ compile templateBodyCompiler
 
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  dateField "date" "%B %e, %Y" <> defaultContext
 
 tailwindCssCompiler :: Compiler (Item String)
 tailwindCssCompiler = do
-    inFile <- getResourceFilePath
-    TmpFile tmp <- newTmpFile "tailwind.out.XXX"
-    tailwindOutput <- unsafeCompiler $ do
-      callTailwind inFile tmp tailwindConfig
-      readFile tmp
-    makeItem tailwindOutput
-  where
-    tailwindConfig :: FilePath
-    tailwindConfig = "./src/tailwind.config.js"
+  inFile <- getResourceFilePath
+  TmpFile tmp <- newTmpFile "tailwind.out.XXX"
+  tailwindOutput <- unsafeCompiler $ do
+    callTailwind inFile tmp tailwindConfig
+    readFile tmp
+  makeItem tailwindOutput
+    where
+      tailwindConfig :: FilePath
+      tailwindConfig = "./src/tailwind.config.js"
 
-    callTailwind :: FilePath -> FilePath -> FilePath -> IO ()
-    callTailwind input output config = callProcess "tailwindcss"
-      [ "--input", input
-      , "--output", output
-      , "--config", config
-      , "--minify"
-      ]
+      callTailwind :: FilePath -> FilePath -> FilePath -> IO ()
+      callTailwind input output config = callProcess "tailwindcss"
+        [ "--input", input
+        , "--output", output
+        , "--config", config
+        , "--minify"
+       ]
